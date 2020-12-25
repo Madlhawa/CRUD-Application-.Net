69141e508e1b69db358e1e8d23215ff0993ef0ce
Madlhawa <madhawa242@gmail.com>
2020-12-26 04:51:44 +0530

    Example 2

---
+++ CRUD Example 2/CRUD Example 2/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace CRUD_Example_2
+{
+    partial class CRUDApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CRUDApp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgvContacts = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(143, 48);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(164, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(30, 195);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(82, 32);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(143, 89);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(164, 20);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(127, 195);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 32);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(226, 195);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(83, 32);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(143, 130);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(164, 20);
+            this.txtCity.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "City";
+            // 
+            // dgvContacts
+            // 
+            this.dgvContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContacts.Location = new System.Drawing.Point(355, 14);
+            this.dgvContacts.Name = "dgvContacts";
+            this.dgvContacts.Size = new System.Drawing.Size(575, 249);
+            this.dgvContacts.TabIndex = 9;
+            this.dgvContacts.DoubleClick += new System.EventHandler(this.dgvContacts_DoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(2, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(29, 23);
+            this.button1.TabIndex = 10;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CRUDApp
+            // 
+            this.ClientSize = new System.Drawing.Size(942, 275);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvContacts);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "CRUDApp";
+            this.Load += new System.EventHandler(this.CRUDApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgvContacts;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CRUD Example 2/CRUD Example 2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace CRUD_Example_2
+{
+    public partial class CRUDApp : Form
+    {
+        EX_Contact contact = new EX_Contact(); 
+        public CRUDApp()
+        {
+            InitializeComponent();
+        }
+
+        void Clear()
+        {
+            txtCity.Text = txtName.Text = txtNumber.Text = "";
+            contact.ContactID = 0;
+            btnCreate.Text = "Create";
+            btnDelete.Enabled = false;
+        }
+        void Populate()
+        {
+            using (DBEntities db = new DBEntities())
+            {
+                dgvContacts.DataSource = db.EX_Contact.ToList();
+            }
+        }
+
+        private void CRUDApp_Load(object sender, EventArgs e)
+        {
+            Clear();
+            Populate();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            contact.Name = txtName.Text.Trim();
+            contact.MobileNumber = txtNumber.Text.Trim();
+            contact.Address = txtCity.Text.Trim();
+            using (DBEntities db = new DBEntities())
+            {
+                if (contact.ContactID == 0)
+                    db.EX_Contact.Add(contact);
+                else
+                    db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            Clear();
+            Populate();
+            MessageBox.Show("Contact Created");
+        }
+
+        private void dgvContacts_DoubleClick(object sender, EventArgs e)
+        {
+            if (dgvContacts.CurrentRow.Index != -1)
+            {
+                contact.ContactID = Convert.ToInt32(dgvContacts.CurrentRow.Cells[0].Value);
+                using (DBEntities db = new DBEntities())
+                {
+                    contact = db.EX_Contact.Where(x => x.ContactID == contact.ContactID).FirstOrDefault();
+                    txtName.Text = contact.Name;
+                    txtNumber.Text = contact.MobileNumber;
+                    txtCity.Text = contact.Address;
+                    btnCreate.Text = "Edit";
+                    btnDelete.Enabled = true;
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Are you sure?","EF CRUD Operation",MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (DBEntities db = new DBEntities())
+                {
+                    var entry = db.Entry(contact);
+                    if (entry.State == EntityState.Detached)
+                        db.EX_Contact.Attach(contact);
+                    db.EX_Contact.Remove(contact);
+                    db.SaveChanges();
+                    Clear();
+                    Populate();
+                    MessageBox.Show("Deleted Successfully");
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["DBEntities"].ConnectionString.ToString();
+            MessageBox.Show(str);
+        }
+    }
+}
